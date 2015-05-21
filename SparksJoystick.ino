3effaa41ca9114c2885bf21aa07158af233ee27b
Paul Richard <prichard@creativemachines.com>
2015-05-20 20:46:45 -0700

    Initial commit, working copy. Changing to allow throttle.

---
+++ SparksJoystick.ino
@@ -0,0 +1,25 @@
+float x, y, throttle;
+byte left_motor=64, right_motor=64;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  x=(analogRead(0)-172.0)/725;
+  y=(analogRead(1)-160.0)/695;
+  throttle=(analogRead(2)-90.0)/795;
+  left_motor=(y * throttle * 128);
+  right_motor=(y * throttle * 128);
+  Serial.print("X:");
+  Serial.print(x);
+  Serial.print(" Y:");
+  Serial.print(y);
+  Serial.print(" T:");
+  Serial.println(throttle);
+  Serial.print(left_motor);
+  Serial.print(", ");
+  Serial.println(right_motor);
+  delay(100);
+}
