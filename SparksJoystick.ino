82da6d6ed94220003a111fba5eb6a044cb2eb8f3
Paul Richard <prichard@creativemachines.com>
2015-05-21 18:40:41 -0700

    Added joystick disconnect checking.

--- SparksJoystick.ino
+++ SparksJoystick.ino
@@ -1,6 +1,9 @@
 /*
 X Min:173 Center:547 Max:897
 Y Min:160 Center:511 Max:853
+
+X Min:343 Center:680 Max:930
+Y Min:310 Center:647 Max:895
 */
 
 float x, y, throttle;
@@ -8,20 +11,29 @@ byte left_motor=64, right_motor=64;
 
 void setup() {
   Serial.begin(38400);
-
+  digitalWrite(A0, 1); // Add pull-up to check if joystick attached.
+  digitalWrite(A1, 1);
+  digitalWrite(A2, 1);
 }
 
 void loop() {
-  x=(analogRead(0)-547.0)/362.5;
-  y=(analogRead(1)-511.0)/347.5;
-  throttle=(analogRead(2)-90.0)/795;
-  left_motor=(64+(y * throttle * 64)+(x * throttle * 25 ));
-  right_motor=(64+(y * throttle * 64)-(x * throttle * 25));
-  left_motor=max(1, min(127, left_motor));
-  right_motor=max(1, min(127, right_motor));
-  Serial.write(left_motor);
-  Serial.write(-right_motor);
+  if(analogRead(0)>1000 || analogRead(1)>1000){  //Sanity check to make sure joystick is connected
+    x=0;
+    y=0;
+  }
+  else{
+    x=(analogRead(0)-680.0)/293.5; // X centering and scaling
+    y=(analogRead(1)-647)/292.5; // Y centering and scaling
+  }
+  throttle=(analogRead(2)-200.0)/680; // Throttle centering and scaling
+  left_motor=(64+(y * throttle * 64)+(x * throttle * 25 )); //left motor mapping
+  right_motor=(64+(y * throttle * 64)-(x * throttle * 25)); //right motor mapping
+  left_motor=max(1, min(127, left_motor)); //Motor speed limits. If exceeded motor driver goes full throttle.
+  right_motor=max(1, min(127, right_motor)); 
+  Serial.write(left_motor); 
+  Serial.write(-right_motor); //Motor controller expects 2 bytes, - pushes correct values
   Serial.flush();
   delay(100);
+ 
 }
 
